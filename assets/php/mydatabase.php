aa8d53e7ccdb0b032c57f225b4dd48ffe8f81c46
kikball9 <76593965+kikball9@users.noreply.github.com>
2023-06-02 15:36:17 +0200

    home page | add login page

---
+++ assets/php/mydatabase.php
@@ -0,0 +1,234 @@
+<?php
+  require_once('constants.php');
+
+  class myDatabase{
+    //Attribut
+    private $myPDO;
+    
+    //Constructeur
+    public function __construct(){
+      try
+    {
+      $this->myPDO = new PDO('mysql:host='.DB_SERVER.';dbname='.DB_NAME.';charset=utf8', DB_USER, DB_PASSWORD);
+    }
+    catch (PDOException $exception)
+    {
+      error_log('Connection error: '.$exception->getMessage());
+      header('HTTP/1.1 503 Service Unavailable');
+      exit;
+    }
+    }
+    //Vérifie que l'email et le password correspondent à un utilisateur
+    public function checkUser($email, $password){
+    try
+    {
+      $request = 'SELECT * FROM users WHERE email=:email AND password=SHA1(:password)';
+      $statement = $this->myPDO->prepare($request);
+      $statement->bindParam (':email', $email, PDO::PARAM_STR, 50);
+      $statement->bindParam (':password', $password, PDO::PARAM_STR, 40);
+      $statement->execute();
+      $result = $statement->fetch();
+    }
+    catch (PDOException $exception)
+    {
+      error_log('Request error: '.$exception->getMessage());
+      return false;
+    }
+    if (!$result)
+      return false;
+    return true;
+  }
+
+  //Ajoute un token d'identification à un utilisateur
+  public function addToken($email, $token){
+    try
+    {
+      $request = 'UPDATE users SET token=:token WHERE email=:email';
+      $statement = $this->myPDO->prepare($request);
+      $statement->bindParam(':email', $email, PDO::PARAM_STR, 50);
+      $statement->bindParam(':token', $token, PDO::PARAM_STR, 20);
+      $statement->execute();
+    }
+    catch (PDOException $exception)
+    {
+      error_log('Request error: '.$exception->getMessage());
+      return false;
+    }
+    return true;
+  }
+
+  //Vérifie que le token corresponde bien à un utilisateur
+  public function verifyToken($token)
+  {
+    try
+    {
+      $request = 'SELECT email FROM users WHERE token=:token';
+      $statement = $this->myPDO->prepare($request);
+      $statement->bindParam (':token', $token, PDO::PARAM_STR, 20);
+      $statement->execute();
+      $result = $statement->fetch();
+    }
+    catch (PDOException $exception)
+    {
+      error_log('Request error: '.$exception->getMessage());
+      return false;
+    }
+    if (!$result)
+      return false;
+    return $result['email'];
+  }
+  
+  //Récupere les informations d'un utilisateur
+  public function requestUsers($email){
+    try{
+      $request = "SELECT email, first_name, name_user, date_birth, img_path AS src FROM users WHERE email=:email";
+      $statement = $this->myPDO->prepare($request);
+      $statement->bindParam(":email", $email, PDO::PARAM_STR, 50);
+      $statement->execute();
+      $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+    catch (PDOException $exception)
+    {
+      error_log('Request error: '.$exception->getMessage());
+      return false;
+    }
+    return $result;
+  }
+
+  //Ajoute un utilisateur
+  public function addUser($email, $password, $first_name, $name_user, $date_birth, $img_path){
+    try{
+      $request = "INSERT INTO users(email, password, first_name, name_user, date_birth, img_path) VALUES(:email, SHA1(:password), :first_name, :name_user, :date_birth, :img_path)";
+      $statement = $this->myPDO->prepare($request);
+      $statement->bindParam(":email", $email, PDO::PARAM_STR, 50);
+      $statement->bindParam(":password", $password, PDO::PARAM_STR, 50);
+      $statement->bindParam(":first_name", $first_name, PDO::PARAM_STR, 50);
+      $statement->bindParam(":name_user", $name_user, PDO::PARAM_STR, 50);
+      $statement->bindParam(":date_birth", $date_birth, PDO::PARAM_STR, 50);
+      $statement->bindParam(":img_path", $img_path, PDO::PARAM_STR, 50);
+      $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+    catch (PDOException $exception)
+    {
+      error_log('Request error: '.$exception->getMessage());
+      return false;
+    }
+    return true;
+  }
+
+  //Modifier un utilisateur (sauf l'email)
+  public function modifyUser($email, $password, $first_name, $name_user, $date_birth, $img){
+    try{
+      $request = "UPDATE users SET password=:password, first_naem=:first_name, name_user=:name_user, date_birth=:date_birth, img_path=:img_path WHERE email=:email";
+      $statement = $this->myPDO->prepare($request);
+      $statement->bindParam(":email", $email, PDO::PARAM_STR, 50);
+      $statement->bindParam(":password", $password, PDO::PARAM_STR, 50);
+      $statement->bindParam(":first_name", $first_name, PDO::PARAM_STR, 50);
+      $statement->bindParam(":name_user", $name_user, PDO::PARAM_STR, 50);
+      $statement->bindParam(":date_birth", $date_birth, PDO::PARAM_STR, 50);
+      $statement->bindParam(":img_path", $img, PDO::PARAM_STR, 50);
+      $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+    catch (PDOException $exception)
+    {
+      error_log('Request error: '.$exception->getMessage());
+      return false;
+    }
+    return true;
+  }
+
+  //supprimer un utilisateur
+  public function delUser($email){
+    try
+    {
+      $request = 'DELETE FROM users WHERE email=:email';
+      $statement = $this->myPDO->prepare($request);
+      $statement->bindParam (':email', $email, PDO::PARAM_STR, 50);
+      $statement->execute();
+      $result = $statement->fetch();
+    }
+    catch (PDOException $exception)
+    {
+      error_log('Request error: '.$exception->getMessage());
+      return false;
+    }
+    if (!$result)
+      return false;
+    
+    try
+      {
+      $request = 'DELETE FROM favorites_tracks WHERE email=:email';
+      $statement = $this->myPDO->prepare($request);
+      $statement->bindParam (':email', $email, PDO::PARAM_STR, 50);
+      $statement->execute();
+      $result = $statement->fetch();
+    }
+    catch (PDOException $exception)
+    {
+      error_log('Request error: '.$exception->getMessage());
+      return false;
+    }
+    return true;
+  }
+
+  //Récupérer les musiques favorites d"un utilisateur
+  public function requestFavorites($email){
+      try
+      {
+        $request = 'SELECT id_tracks FROM favorites_tracks WHERE email=:email';
+        $statement = $this->myPDO->prepare($request);
+        $statement->bindParam (':email', $email, PDO::PARAM_STR, 50);
+        $statement->execute();
+        $result = $statement->fetch();
+      }
+      catch (PDOException $exception)
+      {
+        error_log('Request error: '.$exception->getMessage());
+        return false;
+      }
+      if( $result == ""){
+        return $result;
+      }
+      $favorites = array();
+      for ($i=0;$i<sizeof($result);$i++){         
+        try
+        {
+          $request = 'SELECT id_tracks, name_tracks, date_listened, duration, track_path, id_album, id_artist FROM tracks WHERE id_tracks=:id_tracks';
+          $statement = $this->myPDO->prepare($request);
+          $statement->bindParam (':id_tracks', $id_tracks, PDO::PARAM_STR, 50);
+          $statement->execute();
+          $result = $statement->fetch();
+        }
+        catch (PDOException $exception)
+        {
+          error_log('Request error: '.$exception->getMessage());
+          return false;
+        }
+        if (!$result)
+          return false;
+        array_push($favorites, $result);
+      }
+      return $favorites;
+
+  }
+
+  //Ajouter une musique aux favoris d'un utilisateur
+  public function addFavorite($email, $id_tracks){
+    try{
+      $request = "INSERT INTO favorites_tracks(email, id_tracks) VALUES(:email, :id_tracks)";
+      $statement = $this->myPDO->prepare($request);
+      $statement->bindParam(":email", $email, PDO::PARAM_STR, 50);
+      $statement->bindParam(":id_tracks", $id_tracks, PDO::PARAM_STR, 50);
+      $statement->execute();
+      $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+    catch (PDOException $exception)
+    {
+      error_log('Request error: '.$exception->getMessage());
+      return false;
+    }
+    return true;
+  }
+  } 
+
+?>
---
+++ assets/php/request.php
@@ -0,0 +1,204 @@
+<?php
+
+use function PHPSTORM_META\type;
+
+require "mydatabase.php";
+
+//Authentification via l'email et le mot de passe
+function authenticate($db){
+    if (isset($_SERVER["PHP_AUTH_USER"]) && isset($_SERVER["PHP_AUTH_PW"])){
+        $username = $_SERVER["PHP_AUTH_USER"];
+        $password = $_SERVER["PHP_AUTH_PW"];
+        if (!$db->checkUser($username, $password)){
+            header('HTTP/1.1 401 Unauthorized');
+            exit;
+        }
+        else {
+            $token = base64_encode(openssl_random_pseudo_bytes(12));
+            $db->addToken($username, $token);
+            header('Content-Type: text/html; charset=utf-8');
+            header('Cache-control: no-store, no-cache, must-revalidate');
+            header('Pragma: no-cache');
+
+            header('HTTP/1.1 200 OK');
+            echo $token;
+            exit;
+        }
+    }
+    else {
+        header('HTTP/1.1 401 Unauthorized');
+        echo "c";
+        exit;
+    }
+}
+
+//Verification du Token
+function verifyToken($db){
+    $headers = getallheaders();
+    $token = $headers["Authorization"];
+    if (preg_match("/Bearer (.*)/", $token, $tab)){
+        $token = $tab[1];
+        $email = $db->verifyToken($token);
+        if (!$email){
+            header('HTTP/1.1 401 Unauthorized');
+            exit;
+        }
+        return $email;
+    }
+    else{
+        header('HTTP/1.1 401 Unauthorized');
+        exit;
+    }
+}
+
+//Création de l'objet représentant la base de données
+$myDb = new myDatabase;
+//Acquisition des informations de la requête contenuent dans son URL
+$requestMethod = $_SERVER['REQUEST_METHOD'];
+$request = substr($_SERVER['PATH_INFO'], 1);
+$request = explode('/', $request);
+$requestRessource = array_shift($request);
+$id = array_shift($request);
+if ($id == '')
+  $id = NULL;
+//Verification du token et acquisition de l'idendité de l'utilisateur pour chaque requête sauf si l'utilisateur s'authentifie
+if ($requestRessource == "authenticate"){
+    authenticate($myDb);
+}
+else {
+    $email = verifyToken($myDb);
+    if (isset($_COOKIE["email"])){
+        if($email != $_COOKIE["email"]){
+            header('HTTP/1.1 401 Unauthorized');
+            echo json_encode($email);
+            exit;
+        }
+    }
+}
+if($requestMethod == "GET" && $requestRessource == ""){
+    header("HTTP/1.1 200 OK");
+}
+
+/*
+Gestion de la base de données en fonction des requêtes de l'utilisateur:
+*/
+
+//Envoie des données d'un utilisateurs
+else if ($requestMethod == "GET" && $requestRessource == "user"){
+    $myDbReq = $myDb->requestUsers($email);
+    if ($myDbReq == ""){
+        header('HTTP/1.1 200 OK');
+        echo json_encode("");
+    }
+    else if (!$myDbReq){
+        header('HTTP/1.1 400 Bad Request');
+    }
+    else{
+        header('HTTP/1.1 200 OK');
+        echo json_encode($myDbReq);
+    }
+}
+
+//Création d'un utilisateur
+else if ($requestMethod == "POST" && $requestRessource == "user"){
+    if (isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["first_name"]) && isset($_POST["name_user"]) && isset($_POST["date_birth"]) && isset($_FILES["imgUser"])){
+        //Upload de l'image de l'utilisateur
+        $extFile = substr($_FILES["myfile"]["name"], strpos($_FILES["myfile"]["name"], "."));
+        if ($ext == ".jpg" || $ext == ".jpeg" || $ext == ".png" || $ext == ".gif"){
+            $src = $_FILES["myfile"]["tmp_name"];
+            $imgPath = '/var/www/projet/html/assets/img/'.explode("@", $email)[0].time().$ext;
+        }
+        else {
+            header('HTTP/1.1 400 Bad Request (only .jpg, .jpeg, .png, .gif extensions)');
+        }
+        if (!move_uploaded_file($src, $imgPath)){
+            header('HTTP/1.1 400 Bad Request');
+
+        }
+        //Verification champs email et date
+        if (!preg_match("^[0-9]{4}-[0-1][0-9]-[0-3][0-9]$", $_POST["date_birth"]) || !filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){
+            header('HTTP/1.1 400 Bad Request');
+        }
+        $myDbReq = $myDb->addUser($_POST["email"], $_POST["password"], $_POST["first_name"], $_POST["name_user"], $_POST["date_birth"], $imgPath);
+        if(!$myDbReq){
+            header('HTTP/1.1 400 Bad Request');
+        }
+        else{
+            header('HTTP/1.1 200 OK');
+            echo json_encode($myDbReq);
+        }
+    }
+    else{
+        header('HTTP/1.1 400 Bad Request');
+    }
+}
+//Mofification d'un utilisateur
+else if ($requestMethod == "PUT" && $requestRessource == "user"){
+    parse_str(file_get_contents('php://input'), $_PUT);
+    if (isset($_PUT["password"]) && isset($_PUT["first_name"]) && isset($_PUT["name_user"]) && isset($_PUT["date_birth"]) && isset($_PUT["img"])){
+        $myDbReq = $myDb->modifyUser($email, $_PUT["password"], $_PUT["first_name"], $_PUT["name_user"], $_PUT["date_birth"], $_PUT["img"]);
+        if(!$myDbReq){
+            header('HTTP/1.1 400 Bad Request');
+        }
+        else{
+            header('HTTP/1.1 200 OK');
+            echo json_encode($myDbReq);
+        }
+    }
+    else{
+        header('HTTP/1.1 400 Bad Request');
+    }
+}
+//Suppression d'un utilisateur
+else if ($requestMethod == "DELETE" && $requestRessource == "user"){
+    $myDbReq = $myDb->delUser($email);
+    if(!$myDbReq){
+        header('HTTP/1.1 400 Bad Request');
+    }
+    else{
+        header('HTTP/1.1 200 OK');
+        echo json_encode($myDbReq);
+    }
+}
+//Envoie des musiques favories d'un utilisateur
+else if ($requestMethod == "GET" && $requestRessource == "favorites"){
+    $myDbReq = $myDb->requestFavorites($email);
+    if ($myDbReq == ""){
+        header('HTTP/1.1 200 OK');
+        echo json_encode("");
+    }
+    if(!$myDbReq){
+        header('HTTP/1.1 400 Bad Request');
+    }
+    else{
+        header('HTTP/1.1 200 OK');
+        echo json_encode($myDbReq);
+    }
+}
+//Ajout d'une musique favorie d'un utilisateur
+else if ($requestMethod == "POST" && $requestRessource == "favorites"){
+    if (isset($_POST["id_tracks"])){
+        $myDbReq = $myDbReq->addFavorite($email, $_POST["id_tracks"]);
+        if(!$myDbReq){
+            header('HTTP/1.1 400 Bad Request');
+        }
+        else{
+            header('HTTP/1.1 200 OK');
+            echo json_encode($myDbReq);
+        }
+    }
+    else{
+        header('HTTP/1.1 200 OK');
+        echo json_encode($myDbReq);
+    }
+}
+//Si la requête ne correspond à aucun cas traité
+else{
+    header('HTTP/1.1 400 Bad Request');
+}
+
+
+header('Content-Type: text/x-json; charset=utf-8');
+header('Cache-control: no-store, no-cache, must-revalidate');
+header('Pragma: no-cache');
+?>
