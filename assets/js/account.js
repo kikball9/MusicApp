eee6ad58788d798788b0988b66eb1b61a9dfc769
kikball9 <76593965+kikball9@users.noreply.github.com>
2023-06-05 14:01:57 +0200

    test

--- assets/js/account.js
+++ assets/js/account.js
@@ -1,5 +1,8 @@
-/* DELETE COMPTE */
+/* DELETE USER ACCOUNT */
 document.getElementById("del-account-btn").addEventListener("click", () => {
     ajaxRequest("DELETE", "../php/request.php/user", ()=> {return;});
     document.getElementById("home_page").style.display = "none";
 });
+
+/* USER INFORMATION */
+ajaxRequest("GET", "../php/request.php/user", ()=> {return;});
