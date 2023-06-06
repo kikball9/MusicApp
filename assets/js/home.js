64b66afd3fc440012be329373aea9237daefceee
noone <baptiste.taillandier@isen-ouest.yncrea.fr>
2023-06-06 16:52:52 +0200

    Ajout des filtres

--- assets/js/home.js
+++ assets/js/home.js
@@ -133,18 +133,20 @@ function displayFavs(){
 }
 
 function researchTrack(allTracks){
+    var trackFind = false;
+    document.getElementById("research-div").innerHTML += "<h1 class='text-white'>Titres:</h1>"
     for(var i=0;i<allTracks.length;i++){
         if (myResearch == allTracks[i]["name_tracks"]){
             document.getElementById("research-div").innerHTML += '\
-            <li onclick="displayPageTrack('+allTracks[i]["id_tracks"]+')" onmouseover="this.style.cursor=\'pointer\'" value="'+myAlbum["album-tracks"][i]["id_tracks"]+'" class="track-bar list-group-item m-2 w-50 p-0 d-flex p-0 m-0 text-white"> \
-            <img class="img-small" src="'+myAlbum["album-infos"][0]["img_path"]+'" alt="album_img"> \
+            <li onclick="displayPageTrack('+allTracks[i]["id_tracks"]+')" onmouseover="this.style.cursor=\'pointer\'" value="'+allTracks[i]["id_tracks"]+'" class="track-bar list-group-item m-2 w-50 p-0 d-flex p-0 m-0 text-white"> \
+            <img class="img-small" src="'+allTracks[i]["img_album"]+'" alt="album_img"> \
             <div class="text-center title p-1 text-white"> \
                 <ul class="m-0  list-inline" style="list-style: none;"> \
-                    <li class="list-inline-item">'+myAlbum["album-tracks"][i]["name_tracks"]+'</li> \
+                    <li class="list-inline-item">'+allTracks[i]["name_tracks"]+'</li> \
                     <i class="bi bi-dot"></i> \
-                    <li class="list-inline-item">'+myAlbum["album-infos"][0]["name_artist"]+'</li> \
+                    <li class="list-inline-item">'+allTracks[i]["name_artist"]+'</li> \
                     <i class="bi bi-dot"></i> \
-                    <li class="list-inline-item">'+sec2min(myAlbum["album-tracks"][0]["duration"])+'</li> \
+                    <li class="list-inline-item">'+sec2min(allTracks[i]["duration"])+'</li> \
                 </ul> \
             </div> \
             <button class="btn"> \
@@ -154,23 +156,69 @@ function researchTrack(allTracks){
                 <i class="bi bi-three-dots-vertical three-dot"></i> \
             </button> \
           </li>';
+          trackFind = true
         }
+    }
+    if (!trackFind){
+        document.getElementById("research-div").innerHTML += "<h3 class='text-white'>Aucun titre trouvé</h3>"
     }
 }
 
 function researchArtist(allArtists){
+    var artistFind = false;
+    document.getElementById("research-div").innerHTML += "<h1 class='text-white'>Artistes:</h1>"
     for(var i=0;i<allArtists.length;i++){
         if (myResearch == allArtists[i]["artist-infos"][0]["name_artist"]){
-            displayPageArtist(allArtists[i]["artist-infos"][0]["id_artist"], false);
+            document.getElementById("research-div").innerHTML += '<div onclick="displayPageArtist('+allArtists[i]["artist-infos"][0]["id_artist"]+');" onmouseover="this.style.cursor=\'pointer\'" style="border-radius: 25px;" class="container bg-gradient text-white d-flex align-items-center list-group-item"> \
+            <img src="'+allArtists[i]["artist-infos"][0]["img_path"]+'" alt="album_img" class="img-large"> \
+            <div class="w-100" style="margin-left: 1.5rem;"> \
+                <p class="title-detail artist-title" class="mb-0">'+allArtists[i]["artist-infos"][0]["name_artist"]+'</p> \
+                <div class="d-flex  align-items-center list-group-item"> \
+                    <small id="type_artist" class="m-0"> \
+                        '+allArtists[i]["artist-infos"][0]["type_artist"]+' \
+                    </small> \
+                </div> \
+            </div> \
+        </div>';
+        artistFind =true;
         }
+    }
+    if (!artistFind){
+        document.getElementById("research-div").innerHTML += "<h3 class='text-white'>Aucun artiste trouvé</h3>"
     }
 }
 
 function researchAlbum(allAlbums){
-    for(var i=0;i<allArtists.length;i++){
-        if (myResearch == allArtists[i]["album-infos"][0]["name_album"]){
-            displayPageTrack(allArtists[i]["artist-infos"][0]["id_album"], false);
+    var date, newDate;
+    var albumFind = false;
+    document.getElementById("research-div").innerHTML += "<h1 class='text-white'>Albums:</h1>"
+    for(var i=0;i<allAlbums.length;i++){
+        if (myResearch == allAlbums[i]["album-infos"][0]["name_album"]){
+            date= allAlbums[i]["album-infos"][0]["date_published"].split(' ')[0].split("-");
+            newDate = date[2]+"/"+date[1]+"/"+date[0];
+            document.getElementById("research-div").innerHTML += '<div onclick="displayPageAlbum('+allAlbums[i]["album-infos"][0]["id_album"]+')" onmouseover="this.style.cursor=\'pointer\'" style="border-radius: 25px;" class="container bg-gradient text-white d-flex align-items-center list-group-item mb-5"> \
+            <img src="'+allAlbums[i]["album-infos"][0]["img_path"]+'" alt="album_img" class="img-large"> \
+            <div class="w-100" style="margin-left: 1.5rem;"> \
+                <p  class="title-detail">'+allAlbums[i]["album-infos"][0]["name_album"]+'</p> \
+                <div class="d-flex  align-items-center list-group-item"> \
+                    <img  src="'+allAlbums[i]["album-infos"][0]["artist_img"]+'" alt="artist_img" class="img-small" style="clip-path: ellipse(50% 50%); height: 200px; width: 200px;"> \
+                    <small class="w-100 m-lg-4"> \
+                        <ul class="m-0  list-inline" style="list-style: none;"> \
+                            <li class="list-inline-item">'+allAlbums[i]["album-infos"][0]["name_artist"]+'</li> \
+                            <i class="bi bi-dot"></i> \
+                            <li class="list-inline-item">'+allAlbums[i]["album-infos"][0]["style"]+'</li> \
+                            <i class="bi bi-dot"></i> \
+                            <li class="list-inline-item">'+newDate+'</li> \
+                        </ul> \
+                    </small> \
+                    </div> \
+                </div> \
+            </div>';
+            albumFind = true;
         }
+    }
+    if (!albumFind){
+        document.getElementById("research-div").innerHTML += "<h3 class='text-white'>Aucun album trouvé</h3>"
     }
 }
 
